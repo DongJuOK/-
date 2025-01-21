e10a3f0e65f6e1dd0fce0d198ee1e4fa79b38783
DongJuOK <kcary0907@gmail.com>
2025-01-21 19:45:34 +0900

    (2025.1.21) 다형성

---
+++ Program/Goliath.cpp
@@ -0,0 +1,8 @@
+#include "Goliath.h"
+
+Goliath::Goliath()
+{
+	attack = 12;
+	health = 125;
+	defense = 1;
+}
---
+++ Program/SiegeTank.cpp
@@ -0,0 +1,8 @@
+#include "SiegeTank.h"
+
+SiegeTank::SiegeTank()
+{
+	attack = 70;
+	health = 150;
+	defense = 1;
+}
